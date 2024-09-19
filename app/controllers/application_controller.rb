2bc6d93dd1e54ab44b7da09b15f2fad592756835
motohiro-mm <polarbear.dx@gmail.com>
2024-09-19 15:55:02 +0900

    ヘルパーを設定、ログインのコールバックを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  include SessionsHelper
+  before_action :check_logged_in
+
+  def check_logged_in
+    return if current_user
+
+    redirect_to root_path
+  end
 end
