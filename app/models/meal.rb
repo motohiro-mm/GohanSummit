35468bd7556e6a4abb87c025b0a8b7742c0ffed6
motohiro-mm <polarbear.dx@gmail.com>
2024-09-20 15:08:15 +0900

    モデルに関連付けとバリデーションを追加

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Meal < ApplicationRecord
+  belongs_to :meal_plan
+
+  enum :timing, { breakfast: 0, lunch: 1, dinner: 2 }
+
+  validates :timing, presence: true, uniqueness: { scope: :meal_plan_id }
 end
--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class MealPlan < ApplicationRecord
+  belongs_to :family
+  has_many :meals, dependent: :destroy
+
+  validates :meal_date, presence: true, uniqueness: { scope: :family_id }
+
+  accepts_nested_attributes_for :meals, allow_destroy: true, reject_if: :all_blank
+
+  def start_time
+    meal_date
+  end
 end
