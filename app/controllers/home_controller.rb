f0cfd8bfa7c3579683a5f776e27c5489bb8df8b8
motohiro-mm <polarbear.dx@gmail.com>
2024-10-01 20:33:59 +0900

    コントローラにwelcomeアクションを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,9 @@ class HomeController < ApplicationController
   skip_before_action :check_logged_in, only: :index
 
   def index; end
+
+  def welcome
+    family = Family.find_by!(invitation_token: params[:invitation_token])
+    @invitation_token = family.invitation_token
+  end
 end
