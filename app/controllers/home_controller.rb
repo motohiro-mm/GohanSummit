3d7768b6d329c6e43a0aa7c7f9146a9c5e0ceca7
motohiro-mm <polarbear.dx@gmail.com>
2024-10-02 10:52:40 +0900

    viewの追加、コントローラの修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  skip_before_action :check_logged_in, only: :index
+  skip_before_action :check_logged_in
 
-  def index; end
+  def index
+    redirect_to meal_plans_path if current_user.present?
+  end
 
   def welcome
     family = Family.find_by!(invitation_token: params[:invitation_token])
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,14 @@ class SessionsController < ApplicationController
     user.find_or_new_family(request.env['omniauth.params']['invitation_token']) if user.family_id.nil?
     if user.save
       log_in user
-      redirect_to meal_plans_path
+      redirect_to meal_plans_path, notice: 'ログインに成功しました'
     else
-      redirect_to root_path
+      redirect_to root_path, notice: 'ログインに失敗しました'
     end
   end
 
   def destroy
     log_out
-    redirect_to root_path
+    redirect_to root_path, notice: 'ログアウトしました'
   end
 end
