4ef3275f5afd31c5654a97f559b7ca747733d1fa
motohiro-mm <polarbear.dx@gmail.com>
2024-10-02 14:05:56 +0900

    ルーティングとコントローラに追加

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   end
   get 'welcome', to: 'home#welcome'
   get 'terms', to: 'home#terms'
+  get 'privacy', to: 'home#privacy'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
