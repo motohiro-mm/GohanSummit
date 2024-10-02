c993ad230b6aa7492a732cb0229d95717388484b
motohiro-mm <polarbear.dx@gmail.com>
2024-10-02 11:25:26 +0900

    lintを実行し修正した

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module HomeHelper
+  def google_oauth2_path(invitation_token = nil)
+    if invitation_token
+      "/auth/google_oauth2/?invitation_token=#{invitation_token}"
+    else
+      '/auth/google_oauth2'
+    end
+  end
 end
