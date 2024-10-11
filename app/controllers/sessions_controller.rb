e227b2a81906add15a44d17f437726f4c0835d16
motohiro-mm <polarbear.dx@gmail.com>
2024-10-12 00:07:42 +0900

    ログイン状態を管理するメソッドを修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  skip_before_action :check_logged_in, only: :create
+  skip_before_action :authenticate, only: :create
 
   def create
     user = User.find_or_new_from_auth_hash(request.env['omniauth.auth'])
