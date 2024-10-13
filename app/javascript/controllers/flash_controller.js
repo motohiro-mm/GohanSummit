e8ceca8a92a9068e18595fa71ed30036995f8dd1
motohiro-mm <polarbear.dx@gmail.com>
2024-10-13 15:45:36 +0900

    tailwindcss-stimulus-componentを使ってflashの表示を追加

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,7 @@
+import { Application } from "@hotwired/stimulus";
+import { Alert } from "tailwindcss-stimulus-components"
+
+const application = Application.start();
+application.register('flash', Alert)
+
+export default class extends Alert {}
