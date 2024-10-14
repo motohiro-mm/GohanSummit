b4850f14624358b2edc796ac10da3277304a4b06
motohiro-mm <polarbear.dx@gmail.com>
2024-10-14 15:45:29 +0900

    remarkをリアルタイム通信できるようにbroadcastを追加

--- app/models/remark.rb
+++ app/models/remark.rb
@@ -9,6 +9,10 @@ class Remark < ApplicationRecord
 
   enum :remark_type, { proposal: 0, comment: 1 }, validate: true
 
+  after_create_commit -> { broadcast_of_create }
+  after_update_commit -> { broadcast_of_update }
+  after_destroy_commit -> { broadcast_remove_to [meeting_room, 'remarks'] }
+
   def proposal?
     remark_type == 'proposal'
   end
@@ -19,5 +23,23 @@ class Remark < ApplicationRecord
 
   def editable?(target_user)
     user == target_user
+  end
+
+  private
+
+  def broadcast_of_create
+    if proposal?
+      broadcast_prepend_to [meeting_room, 'proposal'], partial: 'remarks/proposal', locals: { remark: self }, target: 'proposals'
+    else
+      broadcast_prepend_to [meeting_room, 'comment'], partial: 'remarks/comment', locals: { remark: self }, target: 'comments'
+    end
+  end
+
+  def broadcast_of_update
+    if proposal?
+      broadcast_replace_to [meeting_room, 'proposal'], partial: 'remarks/proposal', locals: { remark: self }, target: "remark_#{id}"
+    else
+      broadcast_replace_to [meeting_room, 'comment'], partial: 'remarks/comment', locals: { remark: self }, target: "remark_#{id}"
+    end
   end
 end
