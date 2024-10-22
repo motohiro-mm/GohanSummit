2e8b06cac09b093065db73ad11cc063ffcf402f1
motohiro-mm <polarbear.dx@gmail.com>
2024-10-22 23:27:59 +0900

    formにplaceholderを追加

--- app/helpers/remarks_helper.rb
+++ app/helpers/remarks_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module RemarksHelper
+  def remark_placeholder(remark_type)
+    if remark_type == 'proposal'
+      'カレー、おすし、やきとり'
+    elsif remark_type == 'comment'
+      '中華の気分、軽めがいい'
+    end
+  end
 end
