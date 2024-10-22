67ffd42701f49b8ed4f313da33f9f41330144825
motohiro-mm <polarbear.dx@gmail.com>
2024-10-22 20:04:09 +0900

    helperにボタン表示のメソッドを追加

--- app/helpers/meal_plans_helper.rb
+++ app/helpers/meal_plans_helper.rb
@@ -4,4 +4,12 @@ module MealPlansHelper
   def meal_timing(meal_form)
     Meal.timings.key(meal_form.index)
   end
+
+  def button_display
+    if action_name == 'new'
+      '登録'
+    elsif action_name == 'edit'
+      '更新'
+    end
+  end
 end
