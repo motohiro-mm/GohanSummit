355f8f5921bcfb79e66751b5c93f08d75948ca60
motohiro-mm <polarbear.dx@gmail.com>
2024-11-04 15:57:30 +0900

    システムテストを追加・修正

--- spec/system/meal_plans_spec.rb
+++ spec/system/meal_plans_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'MealPlans', type: :system do
   let(:user) { create(:user) }
-  let(:meal_plan) { create(:meal_plan, family: user.family) }
+  let(:meal_plan) { create(:meal_plan, :with_3_meals, family: user.family) }
 
   before do
     log_in_as user
@@ -32,12 +32,10 @@ RSpec.describe 'MealPlans', type: :system do
 
   it '献立を詳細表示する' do
     visit meal_plans_path(start_date: meal_plan.meal_date)
-    click_on 'TestMeal1'
+    click_on 'BreakfastName'
 
     expect(page).to have_text I18n.l(meal_plan.meal_date, format: :long)
-    expect(page).to have_content 'TestMemo1'
-    expect(page).to have_content 'TestMemo2'
-    expect(page).to have_content 'TestMemo3'
+    expect(page).to have_content 'TestMemo'
   end
 
   it '献立を編集する' do
@@ -50,5 +48,12 @@ RSpec.describe 'MealPlans', type: :system do
     click_on '更新'
 
     expect(page).to have_content 'EditName'
+  end
+
+  it '献立を削除する' do
+    visit meal_plan_path(meal_plan)
+    click_on 'この献立を削除する'
+
+    expect(page).to have_content '削除しました'
   end
 end
