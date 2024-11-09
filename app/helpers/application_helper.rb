d414d7c21a15970b99025f11621568714395832a
motohiro-mm <polarbear.dx@gmail.com>
2024-11-09 15:59:31 +0900

    日時表示の修正、今年の場合は年を表示しない

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,10 @@ module ApplicationHelper
     day.strftime('cell%Y%m%d')
   end
 
+  def format_date_length_whether_this_year(date)
+    date.year == Time.zone.today.year ? :medium : :long
+  end
+
   def format_line_break(text)
     safe_join(text.split("\n"), tag.br)
   end
