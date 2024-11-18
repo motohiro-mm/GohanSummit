fa9ac74b5b10b31de7223283c15ddce2ccea42bd
motohiro-mm <polarbear.dx@gmail.com>
2024-11-18 16:57:12 +0900

    desplay_meta_tagsを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,28 @@ module ApplicationHelper
   def format_line_break(text)
     safe_join(text.split("\n"), tag.br)
   end
+
+  def default_meta_tags
+    {
+      site: 'ごはんサミット',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'ごはんサミットは、家族で献立を相談したり、献立表を共有できるアプリです。',
+      keywords: 'ごはんサミット, gohansummit, ごはん, サミット, 会議, kaigi, 献立, 献立表',
+      og: {
+        title: :title,
+        type: 'website',
+        site_name: 'ごはんサミット',
+        description: :description,
+        separator: '|',
+        image: image_url('ogp.png'),
+        url: 'https://gohansummit.com',
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@motohiromm'
+      }
+    }
+  end
 end
