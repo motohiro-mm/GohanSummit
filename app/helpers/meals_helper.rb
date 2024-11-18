c4a4b93bc8ac42b63a2fdfb8d9b5938fabb62b1a
motohiro-mm <polarbear.dx@gmail.com>
2024-11-18 12:07:40 +0900

    placefolderを追加、cssを調整

--- app/helpers/meals_helper.rb
+++ app/helpers/meals_helper.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
 
 module MealsHelper
+  def meal_timing(meal_form)
+    Meal.timings.key(meal_form.index)
+  end
+
+  def meal_name_placeholder(meal_form_index)
+    if meal_form_index == 0
+      'トースト、納豆ごはん'
+    elsif meal_form_index == 1
+      'パスタ、ラーメン'
+    elsif meal_form_index == 2
+      'やきにく、おすし'
+    end
+  end
+
+  def meal_memo_placeholder(meal_form_index)
+    if meal_form_index == 0
+      'おすすめされたトーストを作ってみよう'
+    elsif meal_form_index == 1
+      '会社の近くでさっと食べられるもの'
+    elsif meal_form_index == 2
+      '給料日なので外食！'
+    end
+  end
 end
