46b5949e01943444c23d115bfeb55b2fef93e615
motohiro-mm <polarbear.dx@gmail.com>
2024-11-18 19:03:06 +0900

    seedデータの重複を修正
    [skip ci]

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ DISHES = %w[からあげ やきそば おこのみやき ビビンバ カレー 
 
 COMMENTS = %w[たくさん食べたいな あっさりの気分 時間ないよね なんでもいいな〜 がっつりしたものが食べたい 一から作ってもいいよね 寒そうだからあったかいものが食べたい 何食べたい気分？ 最近肉料理多いよね 簡単に作れるものがいいな].freeze
 
-local_family = Family.find_or_create_by!(invitation_token: 'fbc20240915gohan')
+local_family = Family.find_or_create_by!(invitation_token: 'gohan20240915seed')
 
 user1 = User.find_or_create_by!(uid: '3141592653589793238462643383279') do |u|
   u.name = 'もとみ'
@@ -32,6 +32,8 @@ end
   local_meal_plan.save!
 
   local_meeting_room = MeetingRoom.find_or_create_by!(meal_plan: local_meal_plan, family: local_family)
+
+  next if local_meeting_room.remarks.present?
 
   [user1, user2, user1].each do |user|
     Remark.create!(
