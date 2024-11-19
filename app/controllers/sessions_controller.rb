8fc067ed4e45d6dbc0603cf527bdf3f0f3584ec5
motohiro-mm <polarbear.dx@gmail.com>
2024-11-19 13:33:44 +0900

    current_userをfind_byに修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     session[:welcome_modal] = true if user.new_record?
     if user.save
       session[:user_id] = user.id
-      redirect_to meal_plans_path, notice: 'ログインしました'
+      redirect_to meal_plans_path, notice: 'ログインしました', status: :see_other
     else
       redirect_to root_path, alert: 'ログインに失敗しました'
     end
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
-    redirect_to root_path, notice: 'ログアウトしました'
+    redirect_to root_path, notice: 'ログアウトしました', status: :see_other
   end
 
   def auth_failure
