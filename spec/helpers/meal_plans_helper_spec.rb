4493bb6c3fed0cbda2a3f15fd06952e362b5833c
motohiro-mm <polarbear.dx@gmail.com>
2024-11-22 13:14:03 +0900

    meal_plans_helper_specを作成

--- spec/helpers/meal_plans_helper_spec.rb
+++ spec/helpers/meal_plans_helper_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe MealPlansHelper, type: :helper do
+  describe '#button_display' do
+    it 'newアクションのとき「登録」を返す' do
+      allow(helper).to receive(:action_name).and_return('new')
+      expect(helper.button_display).to eq('登録')
+    end
 
+    it 'editアクションのとき「更新」を返す' do
+      allow(helper).to receive(:action_name).and_return('edit')
+      expect(helper.button_display).to eq('更新')
+    end
+  end
 end
