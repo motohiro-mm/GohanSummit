86d7ac887c5edcf80bc98017054f8ffc19e7c1aa
motohiro-mm <polarbear.dx@gmail.com>
2024-11-22 14:52:25 +0900

    DBのRemarkのcontentに文字数制限を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_22_050917) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_22_054752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_22_050917) do
     t.bigint "user_id"
     t.bigint "meeting_room_id", null: false
     t.integer "remark_type", null: false
-    t.string "content", null: false
+    t.string "content", limit: 50, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["meeting_room_id"], name: "index_remarks_on_meeting_room_id"
