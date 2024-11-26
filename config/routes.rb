043d2567bd77c23279fb3cb8ca7c02de2c03151d
motohiro-mm <polarbear.dx@gmail.com>
2024-11-26 18:53:52 +0900

    余計なファイル、コードを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@
 
 Rails.application.routes.draw do
   root 'home#index'
-
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: 'sessions#auth_failure'
   get 'log_out', to: 'sessions#destroy', as: 'log_out'
-
   resource :user, only: %i[update destroy]
   get 'user', to: 'users#edit', as: 'edit_user'
   get 'family', to: 'families#show', as: 'family'
@@ -21,17 +19,6 @@ Rails.application.routes.draw do
   get 'terms', to: 'home#terms'
   get 'privacy', to: 'home#privacy'
   get 'about', to: 'home#about'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get 'service-worker' => 'rails/pwa#service_worker', as: :pwa_service_worker
   get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'up' => 'rails/health#show', as: :rails_health_check
 end
