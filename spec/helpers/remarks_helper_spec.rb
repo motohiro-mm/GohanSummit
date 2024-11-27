4a57004f69e77c604895645ef790f37cf4a138cf
motohiro-mm <polarbear.dx@gmail.com>
2024-11-27 23:47:17 +0900

    追加したヘルパーのテストを追加した

--- spec/helpers/remarks_helper_spec.rb
+++ spec/helpers/remarks_helper_spec.rb
@@ -31,4 +31,28 @@ RSpec.describe RemarksHelper, type: :helper do
       expect(result).to have_css 'textarea'
     end
   end
+
+  describe '#form_remark' do
+    it 'new_remark_typeがproposalのときproposal_remarkを返す' do
+      expect(form_remark(Remark.new, 'proposal')).to eq 'proposal_remark'
+    end
+
+    it 'new_remark_typeがcommentのときcomment_remarkを返す' do
+      expect(form_remark(Remark.new, 'comment')).to eq 'comment_remark'
+    end
+
+    it 'new_remark_typeをとらないときRemarkインスタンスを返す' do
+      expect(form_remark(Remark.new)).to be_an_instance_of(Remark)
+    end
+  end
+
+  describe '#new_button_content' do
+    it 'proposalのとき「提案を登録する」が返る' do
+      expect(new_button_content('proposal')).to eq '提案を登録する'
+    end
+
+    it 'commentのとき「コメントを投稿する」が返る' do
+      expect(new_button_content('comment')).to eq 'コメントを投稿する'
+    end
+  end
 end
