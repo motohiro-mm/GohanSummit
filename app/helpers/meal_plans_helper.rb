55fc4ab0f592791f9b2c1f7e9a26273a4e875f3f
motohiro-mm <polarbear.dx@gmail.com>
2024-11-30 23:10:46 +0900

    献立が決まっていないところに未確定を表示、リンク先の変更、カウンターの追加

--- app/helpers/meal_plans_helper.rb
+++ app/helpers/meal_plans_helper.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
 module MealPlansHelper
+  def meal_plan_link(meal_plan, date)
+    if meal_plan&.meals.blank?
+      new_meal_plan_path(meal_date: date)
+    else
+      meal_plan
+    end
+  end
+
+  def meals_length(meal_plan)
+    meal_plan&.meals&.length.to_i
+  end
+
+  def proposals_length(meal_plan)
+    meal_plan&.meeting_room&.remarks&.proposals&.length.to_i
+  end
+
+  def comments_length(meal_plan)
+    meal_plan&.meeting_room&.remarks&.comments&.length.to_i
+  end
+
   def button_display
     if action_name == 'new'
       I18n.t 'helpers.submit.create'
