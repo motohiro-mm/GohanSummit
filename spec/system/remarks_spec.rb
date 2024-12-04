372c0f9e6c4e761d5f4814dc4d880f8b048fbfff
motohiro-mm <polarbear.dx@gmail.com>
2024-12-04 18:39:19 +0900

    表示を待つように修正

--- spec/system/remarks_spec.rb
+++ spec/system/remarks_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Remarks', type: :system do
   let(:meal_plan) { create(:meal_plan, :with_breakfast_and_dinner, family: user.family) }
   let(:meeting_room) { create(:meeting_room, family: user.family, meal_plan:) }
   let(:proposal) { create(:remark, remark_type: 0, content: 'カレー', meeting_room:, user:) }
-  let(:comment) { create(:remark, remark_type: 1, content: '時間ない', meeting_room:, user:) }
+  let(:comment) { create(:remark, remark_type: 1, content: '時間がないね', meeting_room:, user:) }
 
   before do
     log_in_as user
@@ -99,21 +99,22 @@ RSpec.describe 'Remarks', type: :system do
   describe 'コメント' do
     it '新規作成する', :js do
       visit meeting_room_path(meeting_room)
-      expect(page).to have_css 'h2', text: '献立の提案'
+      expect(page).to have_css 'h2', text: 'コメント'
       click_on 'コメントする'
 
-      fill_in 'remark_content', with: '時間ない'
+      fill_in 'remark_content', with: '時間がないね'
       find_by_id('submit_button').click
 
       expect(page).to have_content '投稿しました'
+      expect(page).to have_content Time.zone.today.strftime('%Y/%m/%d')
       within '#comments' do
-        expect(page).to have_content '時間ない'
+        expect(page).to have_content '時間がないね'
       end
     end
 
     it '新規作成時に未入力のままチェックボタンを押すとエラーが出る', :js do
       visit meeting_room_path(meeting_room)
-      expect(page).to have_css 'h2', text: '献立の提案'
+      expect(page).to have_css 'h2', text: 'コメント'
       click_on 'コメントする'
 
       find_by_id('submit_button').click
@@ -122,7 +123,7 @@ RSpec.describe 'Remarks', type: :system do
 
     it '新規作成時に50文字以上入力してチェックボタンを押すとエラーが出る', :js do
       visit meeting_room_path(meeting_room)
-      expect(page).to have_css 'h2', text: '献立の提案'
+      expect(page).to have_css 'h2', text: 'コメント'
       click_on 'コメントする'
 
       fill_in 'remark_content', with: '0' * 51
@@ -133,22 +134,22 @@ RSpec.describe 'Remarks', type: :system do
     it '編集する', :js do
       comment
       visit meeting_room_path(meeting_room)
-      expect(page).to have_css 'h2', text: '献立の提案'
-      click_on '時間ない'
+      expect(page).to have_css 'h2', text: 'コメント'
+      click_on '時間がないね'
 
       fill_in 'remark_content', with: '楽したい'
       find_by_id('submit_button').click
 
       expect(page).to have_content '更新しました'
-      expect(page).to have_no_content '時間ない'
+      expect(page).to have_no_content '時間がないね'
       expect(page).to have_content '楽したい'
     end
 
     it '編集時に入力を削除してチェックボタンを押すとエラーが出る', :js do
       comment
       visit meeting_room_path(meeting_room)
-      expect(page).to have_css 'h2', text: '献立の提案'
-      click_on '時間ない'
+      expect(page).to have_css 'h2', text: 'コメント'
+      click_on '時間がないね'
 
       fill_in 'remark_content', with: ''
       find_by_id('submit_button').click
@@ -158,8 +159,8 @@ RSpec.describe 'Remarks', type: :system do
     it '編集時に50文字以上入力してチェックボタンを押すとエラーが出る', :js do
       comment
       visit meeting_room_path(meeting_room)
-      expect(page).to have_css 'h2', text: '献立の提案'
-      click_on '時間ない'
+      expect(page).to have_css 'h2', text: 'コメント'
+      click_on '時間がないね'
 
       fill_in 'remark_content', with: '0' * 51
       find_by_id('submit_button').click
@@ -169,12 +170,12 @@ RSpec.describe 'Remarks', type: :system do
     it '削除する', :js do
       comment
       visit meeting_room_path(meeting_room)
-      expect(page).to have_css 'h2', text: '献立の提案'
-      click_on '時間ない'
+      expect(page).to have_css 'h2', text: 'コメント'
+      click_on '時間がないね'
       find_by_id('trash_button').click
 
       expect(page).to have_content '削除しました'
-      expect(page).to have_no_content '簡単に作れるものにしよう'
+      expect(page).to have_no_content '時間がないね'
     end
   end
 end
